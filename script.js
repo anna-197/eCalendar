ed644de44ba7df8f57c2d15210a4fdf2ed80e56c
Ananya Nair <an.nair1702@gmail.com>
2022-09-26 19:21:44 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+ 
+        function openpopup() {
+            document.getElementById("mypopup").style.display = "block";
+          }
+          
+          function closepopup() {
+            document.getElementById("mypopup").style.display = "none";
+          }
+
+          function openmonths() {
+            document.getElementById("dropup-content").style.display="block";
+          }
+
+          function opendays() {
+            document.getElementById("dropup-content-days").style.display="block";
+          }
+
+          function openmonths2() {
+            document.getElementById("dropup-content2").style.display="block";
+          }
+
+          function opendays2() {
+            document.getElementById("dropup-content-days2").style.display="block";
+          }
